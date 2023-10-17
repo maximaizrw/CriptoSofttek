20a5b019a8a38dd10826c6cdb1c126e9567ee63a
Maximiliano Maiz <maximaizrw@gmail.com>
2023-10-17 15:31:06 -0300

    Estructuracion

---
+++ CriptoSofttek/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using CriptoSofttek.DataAccess.DatabaseSeeding;
+using CriptoSofttek.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CriptoSofttek.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var seeders = new List<IEntitySeeder>
+            {
+                new UserSeeder(),
+            };
+
+            foreach (var seeder in seeders)
+            {
+                seeder.SeedDatabse(modelBuilder);
+            }
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
